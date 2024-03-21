1508401190d1d27b983665cc846208c2afd862ad
xiguapig <1278493789@qq.com>
2024-03-21 11:09:57 +0800

    更新数据预处理模块

---
+++ 数据预处理/数据变换/归一化.py
@@ -0,0 +1,37 @@
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
+
+
+def MaxMinNormalizetion(x):
+    shapeX = x.shape
+    rows = shapeX[0]
+    cols = shapeX[1]
+    headers = list(x)
+    result = pd.DataFrame(columns=headers)
+    for i in range(0, rows, 1):
+        dict1 = {}
+        dict1[headers[0]] = x['No'][i]
+        for j in range(1, cols, 1):
+            maxcol = x[headers[j]].max()
+            mincol = x[headers[j]].min()
+            val = (x.iloc[i, j] - mincol) / (maxcol - mincol)  # 一般是(x-min)/(max-min)进行归一化处理
+            dict1[headers[j]] = val
+        result = result.append(dict1, ignore_index=True)
+    return result
+
+
+data1 = pd.read_csv(r'CatInfo.csv')
+print('original data\n', data1)
+newdata = MaxMinNormalizetion(data1)
+print('归一化的数据\n', newdata)
+
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
+
+print("去除空值并且归一化处理")
+y = data1.dropna(axis=0).iloc[:, 1:]  # 去除空值
+min_max_scaler = preprocessing.MinMaxScaler()
+x_minmax = min_max_scaler.fit_transform(y)
+print(x_minmax)
