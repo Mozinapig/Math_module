7fd0a54196764e5b5c7c74597aa6a5166d27825f
xiguapig <1278493789@qq.com>
2024-04-05 21:06:30 +0800

    更新预测类算法

---
+++ 预测算法/时序预测分析.py
@@ -0,0 +1,14 @@
+# -*- encoding: utf-8 -*-
+"""
+@Modify Time : 2024/4/5 17:12      
+@Author : Mozinapig   
+@Version : 1.0  
+@Desciption : None
+  
+"""
+# 短期预测用时序，长期预测用回归分析，预测需要做灵敏度分析
+
+#ARIMA预测代码在自己文件夹中（预测一）
+
+
+
